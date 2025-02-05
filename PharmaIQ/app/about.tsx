ec85119e6cf42db86cb00edab8ce534ef0981477
Rabeez Riaz <ruoter123@gmail.com>
2025-02-05 20:59:44 +0500

    Cleanup and consistent heirarchy

--- PharmaIQ/app/about.tsx
+++ PharmaIQ/app/about.tsx
@@ -1,5 +1,12 @@
 import { View } from "react-native";
 
 export default function Page() {
-  return <View className="flex flex-1">about</View>;
+  return (
+    <View className="flex flex-1">
+      <Content />
+    </View>
+  );
+}
+function Content() {
+  return <View className="text-center">about page</View>;
 }
--- PharmaIQ/app/index.tsx
+++ PharmaIQ/app/index.tsx
@@ -12,30 +12,28 @@ export default function Page() {
 
 function Content() {
   return (
-    <View className="flex-1">
-      <View className="py-12 md:py-24 lg:py-32 xl:py-48">
-        <View className="px-4 md:px-6">
-          <View className="flex flex-col items-center gap-4 text-center">
-            <Text
-              role="heading"
-              className="text-center text-3xl font-bold tracking-tighter color-red-400 native:text-5xl sm:text-4xl md:text-5xl lg:text-6xl"
-            >
-              Welcome to PharmaIQ
-            </Text>
-            <Text className="mx-auto max-w-[700px] text-center text-lg text-gray-500 md:text-xl dark:text-gray-400">
-              Discover and collaborate on drug information. Explore our services
-              now.
-            </Text>
+    <View className="py-12 md:py-24 lg:py-32 xl:py-48">
+      <View className="px-4 md:px-6">
+        <View className="flex flex-col items-center gap-4 text-center">
+          <Text
+            role="heading"
+            className="text-center text-3xl font-bold tracking-tighter color-red-400 native:text-5xl sm:text-4xl md:text-5xl lg:text-6xl"
+          >
+            Welcome to PharmaIQ
+          </Text>
+          <Text className="mx-auto max-w-[700px] text-center text-lg text-gray-500 md:text-xl dark:text-gray-400">
+            Discover and collaborate on drug information. Explore our services
+            now.
+          </Text>
 
-            <View className="gap-4">
-              <Link
-                suppressHighlighting
-                className="web:shadow web:focus-visible:outline-none web:focus-visible:ring-1 flex h-9 items-center justify-center overflow-hidden rounded-md bg-gray-900 px-4 py-2 text-sm font-medium text-gray-50 transition-colors hover:bg-gray-900/90 focus-visible:ring-gray-950 active:bg-gray-400/90 disabled:opacity-50 disabled:pointer-events-none ios:shadow dark:bg-gray-50 dark:text-gray-900 dark:hover:bg-gray-50/90 dark:focus-visible:ring-gray-300"
-                href="/"
-              >
-                Explore
-              </Link>
-            </View>
+          <View className="gap-4">
+            <Link
+              suppressHighlighting
+              className="web:shadow web:focus-visible:outline-none web:focus-visible:ring-1 flex h-9 items-center justify-center overflow-hidden rounded-md bg-gray-900 px-4 py-2 text-sm font-medium text-gray-50 transition-colors hover:bg-gray-900/90 focus-visible:ring-gray-950 active:bg-gray-400/90 disabled:opacity-50 disabled:pointer-events-none ios:shadow dark:bg-gray-50 dark:text-gray-900 dark:hover:bg-gray-50/90 dark:focus-visible:ring-gray-300"
+              href="/"
+            >
+              Explore
+            </Link>
           </View>
         </View>
       </View>
