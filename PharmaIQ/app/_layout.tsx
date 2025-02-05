6e26032853b75983a0df3a2d42cfde8c12b4cf57
Rabeez Riaz <ruoter123@gmail.com>
2025-02-05 21:09:35 +0500

    Navbar format

--- PharmaIQ/app/_layout.tsx
+++ PharmaIQ/app/_layout.tsx
@@ -18,9 +18,9 @@ function Header() {
   return (
     <View style={{ paddingTop: top }}>
       <View className="flex h-14 flex-row items-center justify-between px-4 lg:px-6 ">
-        <Link className="flex-1 items-center justify-center font-bold" href="/">
-          PharmaIQ
-        </Link>
+        <View className="flex-1 items-start justify-center font-bold">
+          <Link href="/">PharmaIQ</Link>
+        </View>
         <View className="flex flex-row gap-4 sm:gap-6">
           <Link
             className="text-md web:underline-offset-4 font-medium hover:underline"
