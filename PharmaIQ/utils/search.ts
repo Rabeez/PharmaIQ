ac3a3c62ef8eb35119947fd53b89ab733205e8f4
Rabeez Riaz <ruoter123@gmail.com>
2025-02-11 04:35:58 +0500

    TODO

--- PharmaIQ/utils/search.ts
+++ PharmaIQ/utils/search.ts
@@ -6,6 +6,8 @@ export interface SearchRecord {
 }
 
 export function fuzzySearch(text: string, data: SearchRecord[]) {
+  // TODO: see if there is option to skip any records with less-than threshold score
+  // to avoid making extra large search results list with bad results
   const options = {
     isCaseSensitive: false,
     includeScore: true,
